4a2b3144df40fce80ed30a597cf6817f45ec939a
Benjamín López <benjaminlh2406@gmail.com>
2025-06-04 18:05:16 -0300

    Add files via upload
    Fuente similar a la de la pagina CTC añadida

--- Obligatorio Grupal Programación 1/js/script.js
+++ Obligatorio Grupal Programación 1/js/script.js
@@ -10,8 +10,8 @@ function cargarEventos(eventos) {
 cargarEventos(eventos);
 
 function mostrarEvento(evento) {
-    return`<div class="cartaEvento">
-            <div class ="imgEvento"><img src='${evento.imagen}'</img></div>
+    return `<div class="cartaEvento">
+            <div class="imgEvento"><img src='${evento.imagen}'</img></div>
             <div class="evento"><p>${evento.tipo}</p></div>
             <div class="precio"><p>$ ${evento.precio}</p></div>
             <div class="comprar"><button id="${evento.codigo}">Comprar</button></div>
