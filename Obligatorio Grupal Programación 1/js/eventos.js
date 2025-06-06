47a798c9f1c7b72438c3955b1f1e94e9f78ecaa0
Benjamín López <benjaminlh2406@gmail.com>
2025-06-06 00:09:34 -0300

    Add files via upload
    Filtrado de categoría funcional añadido y filtrado por persona (no funcional) añadido

--- Obligatorio Grupal Programación 1/js/eventos.js
+++ Obligatorio Grupal Programación 1/js/eventos.js
@@ -1,12 +1,12 @@
 let carritoEventos = [];
 
 let eventos = [
-    { imagen: "./images/obra_1.png", codigo: 1, tipo: "Un Viaje En El Tiempo", precio: 1500},
-    { imagen: "./images/obra_2.png", codigo: 2, tipo: "Las Chicas Solo Buscan Divertirse", precio: 1750},
-    { imagen: "./images/obra_3.png", codigo: 3, tipo: "Tarico On The Rotemberg", precio: 1420},
-    { imagen: "./images/obra_4.png", codigo: 4, tipo: "Los Mosqueteros Del Rey", precio: 1690},
-    { imagen: "./images/obra_5.png", codigo: 5, tipo: "Hermanos En Llamas", precio: 1980},
-    { imagen: "./images/obra_6.png", codigo: 6, tipo: "Eugenio & Culini", precio: 1310},
+    { imagen: "./images/obra_1.png", codigo: 1, tipo: "Un Viaje En El Tiempo", categoria: "Eventos" , precio: 1500},
+    { imagen: "./images/obra_2.png", codigo: 2, tipo: "Las Chicas Solo Buscan Divertirse", categoria: "Eventos" , precio: 1750},
+    { imagen: "./images/obra_3.png", codigo: 3, tipo: "Tarico On The Rotemberg", categoria: "Eventos" , precio: 1420},
+    { imagen: "./images/obra_4.png", codigo: 4, tipo: "Los Mosqueteros Del Rey", categoria: "Deportes" , precio: 1690},
+    { imagen: "./images/obra_5.png", codigo: 5, tipo: "Hermanos En Llamas", categoria: "Eventos" , precio: 1980},
+    { imagen: "./images/obra_6.png", codigo: 6, tipo: "Eugenio & Culini", categoria: "Conciertos" , precio: 1310},
 ];
 
 let personas = [
@@ -20,3 +20,7 @@ let evento = {
     tipo:"",
     precio:0,
 };
+
+
+
+
