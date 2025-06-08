f300c0e90882a68ad02d8ff03b2c20e631e0cbe1
Benjamín López <benjaminlh2406@gmail.com>
2025-06-08 00:57:34 -0300

    Add files via upload
    Estilo ajustado y pagina para dar de alta eventos añadida, no funciona por el momento.

---
+++ js/script.js
@@ -0,0 +1,35 @@
+let container = document.querySelector(".container");
+let filtroCategoria = document.getElementById("seleccionarCategoria");
+
+function cargarEventos(eventos) {
+    container.innerHTML = "";
+    eventos.forEach(evento => {
+        container.innerHTML += mostrarEvento(evento);
+    });
+}
+
+cargarEventos(eventos);
+
+function mostrarEvento(evento) {
+    return `<div class="cartaEvento">
+            <div class="imgEvento"><img src='${evento.imagen}'></div>
+            <p class="categoria">${evento.categoria}</p>
+            <div class="eventoTexto">
+            <p class="evento">${evento.tipo}</p>
+            <p class="precio">$ ${evento.precio}</p>
+            <button class="comprar" id="${evento.codigo}">Comprar</button>
+            </div>`;
+}
+
+/* Filtrar por categoría */
+filtroCategoria.addEventListener("change", () => {
+    let categoria = filtroCategoria.value;
+    let categoriaFiltrada = [];
+
+    if (categoria === "Todos") {
+        categoriaFiltrada = eventos;
+    } else {
+        categoriaFiltrada = eventos.filter(evento => evento.categoria === categoria);
+    }
+    cargarEventos(categoriaFiltrada);
+})
