0e1846f18b75bf2c59d90fadd1d587b2e62a33a7
Benjamín López <benjaminlh2406@gmail.com>
2025-06-22 02:16:36 -0300

    Modificación y baja de eventos añadida

--- Obligatorio Grupal Programación 1/js/eventos.js
+++ Obligatorio Grupal Programación 1/js/eventos.js
@@ -1,22 +1,18 @@
+import { Evento } from './claseEventos.js';
+
 let carritoEventos = [];
 
-let eventos = [
-    { imagen: "./images/obra_1.png", codigo: 1, tipo: "Un Viaje En El Tiempo", categoria: "Eventos" , precio: 1500},
-    { imagen: "./images/concierto_1.png", codigo: 2, tipo: "Wonder Matinée", categoria: "Conciertos", precio: 1375},
-    { imagen: "./images/obra_2.png", codigo: 3, tipo: "Las Chicas Solo Buscan Divertirse", categoria: "Eventos" , precio: 1750},
-    { imagen: "./images/deporte_1.png", codigo: 4, tipo: "Cruz Azul vs América", categoria: "Deportes" , precio: 1750},
-    { imagen: "./images/obra_3.png", codigo: 5, tipo: "Tarico On The Rotemberg", categoria: "Eventos" , precio: 1420},
-    { imagen: "./images/obra_4.png", codigo: 6, tipo: "Los Mosqueteros Del Rey", categoria: "Eventos" , precio: 1690},
-    { imagen: "./images/concierto_2.png", codigo: 7, tipo: "Polenta", categoria: "Conciertos" , precio: 1220},
-    { imagen: "./images/obra_5.png", codigo: 8, tipo: "Hermanos En Llamas", categoria: "Eventos" , precio: 1980},
-    { imagen: "./images/obra_6.png", codigo: 9, tipo: "Eugenio & Culini", categoria: "Eventos" , precio: 1310},
-];
-
-let personas = [
-    { id: 0, nombre: "Benjamín", eventos: []},
-    { id: 1, nombre: "Felipe", eventos: []},
-    { id: 2, nombre: "Carlos", eventos: []},
-];
+export let eventos = [
+    new Evento("./images/obra_1.png", 1, "Un Viaje En El Tiempo", "Eventos", 1500, 0),
+    new Evento("./images/concierto_1.png", 2, "Wonder Matinée", "Conciertos", 1375, 0),
+    new Evento("./images/obra_2.png", 3, "Las Chicas Solo Buscan Divertirse", "Eventos", 1750, 0),
+    new Evento("./images/deporte_1.png", 4, "Cruz Azul vs América", "Deportes", 1750, 0),
+    new Evento("./images/obra_3.png", 5, "Tarico On The Rotemberg", "Eventos", 1420, 0),
+    new Evento("./images/obra_4.png", 6, "Los Mosqueteros Del Rey", "Eventos" , 1690, 0),
+    new Evento("./images/concierto_2.png", 7, "Polenta", "Conciertos", 1220, 0),
+    new Evento("./images/obra_5.png", 8, "Hermanos En Llamas", "Eventos", 1980, 0),
+    new Evento("./images/obra_6.png", 9, "Eugenio & Culini", "Eventos", 1310, 0),
+]
 
 let evento = {
     codigo:0,
@@ -24,7 +20,3 @@ let evento = {
     categoria:"",
     precio:0,
 };
-
-
-
-
--- Obligatorio Grupal Programación 1/js/script.js
+++ Obligatorio Grupal Programación 1/js/script.js
@@ -1,8 +1,12 @@
+import { eventos } from "./eventos.js";
+
 let container = document.querySelector(".container");
 let filtroCategoria = document.getElementById("seleccionarCategoria");
 
 function cargarEventos(eventos) {
+    //Se limpia el div
     container.innerHTML = "";
+    //Por cada evento se le agrega al div los elementos que crea la funcion mostrarEventos
     eventos.forEach(evento => {
         container.innerHTML += mostrarEvento(evento);
     });
@@ -32,20 +36,86 @@ function mostrarEvento(evento) {
             </div>`;
 }
 
+document.querySelector('.container').addEventListener('click', function(e) {
+    if (e.target.classList.contains('comprar')) {
+        let tipo = document.querySelector('.tipo').value;
+        let precio = document.querySelector('.importe').value;
+
+        let eventoCarrito = {
+            tipo: tipo,
+            precio: parseInt(precio)
+        };
+
+        let idPersona = document.getElementById('seleccionarPersona').value;
+        let carrito = obtenerCarritoDeLS(idPersona);
+        carrito.push(eventoCarrito);
+        guardarCarritoEnLS(idPersona, carrito);
+        mostrarCarrito(carrito);
+        alert('Evento agregado al carrito');
+    }
+});
+
 // Filtrar por categoría
+
+//Cuando el usuario cambia de opción se ejecuta la función
 filtroCategoria.addEventListener("change", () => {
+    //Se guarda la opcion elegida en una variable
     let categoria = filtroCategoria.value;
+    //Se obtienen los eventos del LS y se guardan en una variable
     let eventosAlmacenados = obtenerEventosDeLocalStorage();
+    //Si no hay eventos en el LS se llama a los valores por defecto
     if (eventosAlmacenados.length === 0) {
         eventosAlmacenados = eventos;
     };
 
     let categoriaFiltrada = [];
-
+    //Si la opción seleccionada es "Todos" se muestran todos los eventos que hay en el LS
     if (categoria === "Todos") {
         categoriaFiltrada = eventosAlmacenados;
     } else {
+        //Sino se le hace un filtro al LS para mostrar los eventos que tengan la misma categoria que la seleccionada
         categoriaFiltrada = eventosAlmacenados.filter(evento => evento.categoria === categoria);
     }
+    //A la funcion cargarEventos se le pasa los eventos ya filtrados
     cargarEventos(categoriaFiltrada);
+})
+
+function guardarCarritoEnLS(idPersona, carrito) {
+    let carritos = JSON.parse(localStorage.getItem('carritos')) || {};
+    carritos[idPersona] = carrito;
+    localStorage.setItem('carritos', JSON.stringify(carritos));
+}
+
+function obtenerCarritoDeLS(idPersona) {
+    let carritos = JSON.parse(localStorage.getItem('carritos')) || {};
+    let carrito = carritos[idPersona];
+    // Si no es un array, devuelve un array vacío
+    return Array.isArray(carrito) ? carrito : [];
+}
+
+function mostrarCarrito(carrito) {
+    let compra = document.querySelector('.carrito');
+    if (!compra) return; // Si no existe, no hace nada
+    
+    compra.innerHTML = ""
+    carrito.forEach(evento => {
+        compra.innerHTML += `
+        <li>
+            <p>Evento: ${evento.tipo}</p>
+            <p>Precio: $${evento.precio}</p>
+            <button class="eliminarCheckout">Eliminar</button>
+        </li>`
+    });
+}
+
+let personas = [
+    { id: 0, nombre: "Benjamín", eventos: []},
+    { id: 1, nombre: "Felipe", eventos: []},
+    { id: 2, nombre: "Carlos", eventos: []},
+];
+
+document.getElementById('seleccionarPersona').addEventListener('change', function() {
+    let idPersona = this.value;
+    let carrito = obtenerCarritoDeLS(idPersona);
+    mostrarCarrito(carrito)
 })
