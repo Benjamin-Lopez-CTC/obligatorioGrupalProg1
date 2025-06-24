4c1ff819229969d4c3d6f3aba0594f67b6138908
Benjamín López <benjaminlh2406@gmail.com>
2025-06-24 00:55:32 -0300

    Versión 1 añadida al codigo fuente

---
+++ Obligatorio Grupal Programación 1 v1.0.0/js/claseEventos.js
@@ -0,0 +1,10 @@
+export class Evento {
+    constructor(imagen, codigo, tipo, categoria, precio, cantidadVendida) {
+        this.imagen = imagen;
+        this.codigo = codigo;
+        this.tipo = tipo;
+        this.categoria = categoria;
+        this.precio = precio;
+        this.cantidadVendida = cantidadVendida;
+    }
+}
